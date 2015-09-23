118198708ef5bc3918cf888c6656a012dd9dcc9b
commenthol <commenthol@gmail.com>
2015-09-23 09:40:25 +0200

    wip

--- test/stats.mocha.js
+++ test/stats.mocha.js
@@ -7,6 +7,7 @@ var compose = require('connect-composer')
 var stats_ = require('../lib/stats')
 var test1 = require('./lib/test1')
 var test2 = require('./lib/test2')
+var function2name = require('../lib/function2name')
 
 describe('stats', function () {
   it('can wrap and run a middleware with arity 3', function (done) {
@@ -19,7 +20,7 @@ describe('stats', function () {
     assert.ok(!test1.fix.stats)
 
     mw(req, res, function () {
-      assert.strictEqual(stats.data[test1.fix].count, 1)
+      assert.strictEqual(stats.data[function2name(test1.fix).string].count, 1)
       done()
     })
   })
@@ -35,7 +36,7 @@ describe('stats', function () {
 
     mw('bad', req, res, function () {
       assert.deepEqual(res, { error: 'bad' })
-      assert.strictEqual(stats.data[test1.err].count, 1)
+      assert.strictEqual(stats.data[function2name(test1.err).string].count, 1)
       done()
     })
   })
@@ -50,7 +51,7 @@ describe('stats', function () {
     assert.ok(mw.stats)
 
     mw(req, res, function () {
-      assert.strictEqual(stats.data[fn].count, 1)
+      assert.strictEqual(stats.data[function2name(fn).string].count, 1)
       done()
     })
   })
@@ -65,28 +66,25 @@ describe('stats', function () {
     mw(req, res, function () {
       // ~ console.log(mw.stack)
       // ~ console.log(stats.data)
-      assert.strictEqual(stats.data[test1.fix].count, 5)
+      assert.strictEqual(stats.data[function2name(test1.fix).string].count, 5)
       done()
     })
   })
 
-  it.only('can add dump stats of a composed middleware', function (done) {
+  it('can run and dump stats of a composed middleware', function (done) {
     var stats = stats_()
     var req = {}
     var res = {}
-    // ~ var mw = compose(test1.nTimes(5, test1.fix), test1.rand, test2)
-
-    var mw = compose(test2)
+    var mw = compose(test1.nTimes(5, test1.fix), test1.rand, test2)
+    // ~ var mw = compose(test2)
     mw.options = { stats: stats.from }
 
     mw(req, res, function () {
-      console.log(mw.stack)
+      // ~ console.log(mw.stack)
       // ~ console.log(stats.data)
       // ~ assert.strictEqual(stats.data[test1.fix].count, 5)
       stats.dump()
       done()
     })
   })
-
-
 })
